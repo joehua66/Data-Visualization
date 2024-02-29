d21adf301993b672fa29640737e9f39bd64695d5
joehua66 <johndoe@example.com>
2024-02-29 13:59:31 -0500

    add crimes

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let data;
 let maxLat,maxLng,mainLat,mainLng;
-let locations=[]
+let crimes=[];
 
 function preload(){
     data = loadJSON('./data.json');
@@ -8,17 +8,21 @@ function preload(){
 
 async function setup(){
 createCanvas(600,600)
-const lats = data.locations.map( location => location[0])
+
+
+
+const lats = data.crimes.map( location => location[0])
+console.log(lats)
 maxLat = Math.max (... lats)
-maxLng = Math.max(...data.locations.map(location => location[1]))
+maxLng = Math.max(...data.crimess.map(location => location[1]))
 minLat = Math.min(...lats)
-minLng = Math.min(...data.locations.map(location => location[1]))
+minLng = Math.min(...data.crimess.map(location => location[1]))
 
 console.log(maxLat,maxLng,minLat,minLng)
 
-data.stations.forEach( s => {
+data.crimes.forEach( s => {
     const location= new Location(s);
-    locations.push(location);
+    crimes.push(location);
 })
 
 
@@ -32,7 +36,7 @@ function draw(){
     background(220)
    
 
-    locations.forEach(s =>{
+    crimes.forEach(s =>{
     s.display();
    })
 

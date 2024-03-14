2ca42d31f0f1d5e348fe74ae639aba48a157237f
joehua66 <johndoe@example.com>
2024-03-14 17:51:37 -0400

    3.14

--- location.js
+++ location.js
@@ -1,14 +1,65 @@
 class Location{
-    constructor(loaction){
+    constructor(crime){
         
-        this.x= map(data.crimes.location[0],minLng,maxLng,0,width)
-        this.y= map(data.crimes.location[1],minLat,maxLat,height,0)
+        this.x= map(crime.location[1],minLng,maxLng,0,width)
+        this.y= map(crime.location[0],minLat,maxLat,height,0)
+        this.datetime=crime.datetime
+        this.des=crime.description
+        this.color=color(255,0,0)
+        this.size=10
+        this.textSize=24
+        //this.color = color(random(255), random(255), random(255));
+        
+
     }
 
     display(){
+       
         noStroke();
-        fill(150);
-        circle(this.x,this.y,3)
+        fill(this.color);
+        circle(this.x,this.y,this.size)
+        //console.log(this.color)
+    }
+
+    mouseOn() {
+        let d = dist(mouseX, mouseY, this.x, this.y);
+        if (d < this.size/2) {
+          // 如果点击到了圆形，改变颜色
+          //this.color = color(random(255), random(255), random(255));
+          this.size=30
+          
+          fill(0)
+          textSize(this.textSize)
+          textFont("monospace", 25);
+          text(this.datetime,this.x+20,this.y-20)
+          textSize(this.textSize/2)
+         text(this.des,this.x+20,this.y+20)
+        
+
+         
+        }
+
+        if (d > this.size/2){
+            this.size=10
+        }
+      
+        //console.log(this.x,this.y)
+        //console.log(mouseX,mouseY)
+      // console.log(d);
+
 
     }
+
+     /*mouseClicked() {
+        let d = dist(mouseX, mouseY, this.x, this.y);
+        fill(0,0,255)
+        textSize(this.textSize)
+        text('hoooo',width/2,height/2)
+       //text(this.des,this.x+20,this.y+20)
+       //console.log(this.des)
+      
+        
+
+
+    }*/
 }
--- sketch.js
+++ sketch.js
@@ -1,43 +1,46 @@
 let data;
-let maxLat,maxLng,minLat,minLng;
-let crimes=[];
+let maxLat, maxLng, minLat, minLng;
+let crimes = [];
+let myMap;
 
-function preload(){
+function preload() {
     data = loadJSON('./data.json');
+    myMap= loadImage('./map.jpeg')
 }
 
-async function setup(){
-createCanvas(600,600)
+async function setup() {
+    createCanvas(1265*0.7, 977*0.7)
 
-data.crimes.forEach( s => {
-    const location= new Location(s);
-    crimes.push(location);
-})
+    const lats = data.crimes.map(crime => crime.location[0])
+    console.log(lats)
+    maxLat = Math.max(...lats)
+    maxLng = Math.max(...data.crimes.map(crime => crime.location[1]))
+    minLat = Math.min(...lats)
+    minLng = Math.min(...data.crimes.map(crime => crime.location[1]))
 
-const lats = data.crimes.map( location => location[0])
-console.log(lats)
-maxLat = Math.max (... lats)
-maxLng = Math.max(...data.crimess.map(location => location[1]))
-minLat = Math.min(...lats)
-minLng = Math.min(...data.crimess.map(location => location[1]))
-
-console.log(maxLat,maxLng,minLat,minLng)
-
-
-
-
+    data.crimes.forEach(s => {
+        const location = new Location(s);
+        crimes.push(location);
+    })
+   // console.log(maxLat, maxLng, minLat, minLng)
 
 }
 
-
-
-
-function draw(){
+function draw() {
     background(220)
-
-    crimes.forEach(s =>{
-    s.display();
-   })
+    
+    image(myMap,0,0,width,height)
+    tint(180)
+    crimes.forEach(s => {
+        s.display();
+        s.mouseOn();
+    })
 
 }
 
+/*function mouseClicked(){
+    crimes.forEach(s => {
+        s.mouseClicked();
+    })
+
+}*/
